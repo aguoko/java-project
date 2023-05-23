2fd1ae34d2cbb9aadffa41780a9da2424b42119d
CodeCommit-user-at-316019785232 <trainaws100@gmail.com>
2023-05-23 17:25:19 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -85,8 +85,8 @@ resource "aws_security_group" "allow_web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    #cidr_blocks = ["${var.my_ip}/32"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.my_ip}/32"]
+    #cidr_blocks = ["0.0.0.0/0"]
 
   }
 
